4f836e07c109a43dfbe5209777a7f9708e2a9c63
Ali Abu Diab <59374676+aliabudiab@users.noreply.github.com>
2023-08-07 14:55:19 +0300

    Update _document.tsx
    Modify Meta Tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,55 +9,14 @@ export default function Document() {
           name="description"
           content="Ali Abu Diab is a software engineer with 3+ years of experience in building and maintaining visually stunning, user-friendly, and responsive websites. Proficient in HTML, CSS, and JavaScript, as well as modern libraries and frameworks."
         />
-        <meta name="revisit-after" content="1 days" />
-        <meta name="robots" content="index, follow" />
-        <meta name="apple-mobile-web-app-title" content="Ali Abu Diab" />
-        <meta name="application-name" content="AD Portfolio" />
         <meta
           name="keywords"
           content="software engineer, frontend engineer, Nuxt, Next, React, Vue, web applications, personal website, Ali Abu Diab, Ali, Abu Diab, علي أبودياب, علي"
         />
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="revisit-after" content="1 day" />
+        <meta name="robots" content="index, follow" />
 
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/ico" href="/favicon.ico" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="192x192"
-          href="/android-chrome-192x192.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="512x512"
-          href="/android-chrome-512x512.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-
-        <meta name="theme-color" content="#000000" />
-        <meta name="language" content="English" />
-        <meta name="owner" content="Ali Abu Diab" />
-        <meta name="copyright" content="Ali Abu Diab(c) 2023" />
-        <meta name="author" content="Ali Abu Diab" />
+        {/* Open Graph Meta Tags */}
         <meta property="og:locale" content="en-US" />
         <meta property="og:title" content="Ali Abu Diab - Software Engineer" />
         <meta
@@ -66,6 +25,28 @@ export default function Document() {
         />
         <meta property="og:url" content="https://aliabudiab.com/" />
         <meta property="og:type" content="website" />
+
+        {/* Favicon and App Icons */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/ico" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+
+        {/* Other Meta Tags */}
+        <meta name="apple-mobile-web-app-title" content="Ali Abu Diab" />
+        <meta name="application-name" content="AD Portfolio" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+        <meta name="theme-color" content="#000000" />
+        <meta name="language" content="English" />
+        <meta name="owner" content="Ali Abu Diab" />
+        <meta name="copyright" content="Ali Abu Diab(c) 2023" />
+        <meta name="author" content="Ali Abu Diab" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
